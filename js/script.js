a9d180410fc21b2bcf42ca46000cd8c55fb34a41
Diego Monroy <diegomonroy.zencode@gmail.com>
2023-03-11 21:05:51 -0500

    [FEAT] add header with responsive navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // script file
+const navigation = document.querySelector(".navigation");
+
+function toggleMenu(event) {
+  event.classList.contains("fa-bars")
+    ? event.classList.replace("fa-bars", "fa-xmark")
+    : event.classList.replace("fa-xmark", "fa-bars");
+  navigation.classList.toggle("top-[120px]");
+}
